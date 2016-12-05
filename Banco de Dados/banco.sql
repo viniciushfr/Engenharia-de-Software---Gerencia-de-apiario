6508ae653d6582258837146a2780ce3e8c19f903
HumbertoPedro <humberto_pedro93@hotmail.com>
2016-12-05 21:31:06 -0200

    Arrumando o banco.sql

--- Banco de Dados/banco.sql
+++ Banco de Dados/banco.sql
@@ -8,9 +8,7 @@ Create Table Cliente (
 	cidade varchar(100)not null,
 	estado varchar(100)not null,
 	idade int not null,
-	
 	constraint pkCliente primary key(idCliente)
-	
 );
 
 Create Table Administrador(
@@ -20,21 +18,18 @@ Create Table Administrador(
 	tipoUsuario  bit not null,
 	telefone varchar(30) not null,
 	nome varchar(100) not null,
-	
 	constraint pkAdministrador primary key(idAdministrador)
 );
 
 Create Table Atendimento(
 	idAtendimento int identity(1,1),
-	id Cliente int not null,
+	idCliente int not null,
 	assunto varchar(100) not null,
 	mensagem varchar(255)not null,
 	situacao varchar(100)not null,
 	dataAtendimento datetime not null,
-	
 	constraint pkAtendimento primary key(idAtendimento),
 	foreign key(idCliente) references Cliente(idCliente)
-	
 );
 
 Create Table Apiario(
@@ -43,7 +38,6 @@ Create Table Apiario(
 	localizacao varchar(255) not null,
 	quantasVezes int not null,
 	hora time not null,
-	
 	constraint pkApiario primary key(idApiario),
 	foreign key(idCliente) references Cliente(idCliente)
 );
@@ -51,31 +45,28 @@ Create Table Apiario(
 Create Table DadosApiario(
 	idDados int identity(1,1),
 	idApiario int not null,
-	temperatura varchar(100) not null,
-	umidade vachar(100),
+	temperatura float not null,
+	umidade float not null,
 	dataDadosApiario datetime not null,
-	
 	constraint pkDadosApiario primary key(idDados),
 	foreign key(idApiario) references Apiario(idApiario)
-	
 );
 
 Create Table Caixa(
 	idCaixa int identity(1,1),
+	idApiario int not null,
 	situacao varchar(255) not null,
 	conexao bit not null,
-	constraint pkCaixa primary key(idCaixa)
-	
+	constraint pkCaixa primary key(idCaixa),
+	foreign key(idApiario) references Apiario(idApiario)	
 );
 
 Create Table DadosCaixa(
 	idDadosCaixa int identity(1,1),
 	idCaixa int not null,
 	peso float not null,
-	fluxoAbelhas double not null,
+	fluxoAbelhas float not null,
 	dataDadosCaixa datetime not null,
-	
 	constraint pkDadosCaixa primary key(idDadosCaixa),
-	foreign key(idCaixa) references (idCaixa)
-	
+	foreign key(idCaixa) references Caixa(idCaixa)
 );
