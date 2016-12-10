5de98782fe65a7750e09bf64066c3b2c9422fbed
Carlos Roberto Garcia Junior <carlos-r-g-junior@hotmail.com>
2016-12-10 00:05:41 -0200

    Criando view cadastrar

---
+++ Software/Apiario/Apiario/Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Apiario.Controllers
+{
+    public class ClienteController : Controller
+    {
+        //
+        // GET: /Cliente/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastrar() 
+        {
+            return View();
+        }
+
+	}
+}
