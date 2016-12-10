d512a77995bdac7e32f0544e2861f731daf2d6cb
Carlos Roberto Garcia Junior <carlos-r-g-junior@hotmail.com>
2016-12-10 02:53:43 -0200

    Alterando a routa de home para inicio

--- Software/Apiario/Apiario/Controllers/InicioController.cs
+++ Software/Apiario/Apiario/Controllers/InicioController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace Apiario.Controllers
 {
-    public class HomeController : Controller
+    public class InicioController : Controller
     {
         public ActionResult Index()
         {
