1522fdf1de057453d442de9ffc0991683eac3423
Carlos Roberto Garcia Junior <carlos-r-g-junior@hotmail.com>
2016-12-10 04:17:32 -0200

    Arrumando bugs

---
+++ Software/Apiario/Apiario/Context/ClienteContext.cs
@@ -0,0 +1,14 @@
+﻿using Apiario.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Apiario.Context
+{
+    public class ClienteContext : DbContext
+    {
+        public DbSet<Cliente> Clientes {get ; set;}
+    }
+}
