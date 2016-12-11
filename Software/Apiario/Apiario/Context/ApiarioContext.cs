cf35d46174a2c7ada2fb113a4d4fbb91b836c556
Carlos Roberto Garcia Junior <carlos-r-g-junior@hotmail.com>
2016-12-11 21:13:10 -0200

    Subindo tudo

---
+++ Software/Apiario/Apiario/Context/ApiarioContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Apiario.Context
+{
+    public class ApiarioContext : DbContext
+    {
+        public DbSet<Apiario.Models.Apiario> Apiarios { get; set; }
+    }
+}
