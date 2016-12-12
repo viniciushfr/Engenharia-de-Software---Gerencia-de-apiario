f27d9c17b2db9a2c0435fe87660a76646a33edcd
Carlos Roberto Garcia Junior <carlos-r-g-junior@hotmail.com>
2016-12-12 15:33:28 -0200

    Criando todos context e adicionando eles no web config, foi apagado um conectstring que tava igual

---
+++ Software/Apiario/Apiario/Context/AtendimentoContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Apiario.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Apiario.Context
+{
+    public class AtendimentoContext : DbContext
+    {
+        public DbSet<Atendimento> Atendimentos { get; set; }
+    }
+}
