cc0a7a8523808241d40146b597750dfbfd69c807
Carlos Roberto Garcia Junior <carlos-r-g-junior@hotmail.com>
2016-12-12 16:00:04 -0200

    Arrumando views

--- Software/Apiario/Apiario/Controllers/InicioController.cs
+++ Software/Apiario/Apiario/Controllers/InicioController.cs
@@ -15,14 +15,12 @@ namespace Apiario.Controllers
 
         public ActionResult Sobre()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contato()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
